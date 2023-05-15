2128d11f368b6e16cbed735b742be50df2b93952
Daniel Grillmeyer <daniel.grillmeyer@stud-mail.uni-wuerzburg.de>
2023-05-15 11:08:34 +0200

    adapted padding in moving_avg module to allow for even kernel sizes

--- layers/Autoformer_EncDec.py
+++ layers/Autoformer_EncDec.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 
 class my_Layernorm(nn.Module):
@@ -28,7 +29,7 @@ class moving_avg(nn.Module):
 
     def forward(self, x):
         # padding on the both ends of time series
-        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        front = x[:, 0:1, :].repeat(1, math.ceil((self.kernel_size - 1) / 2), 1)
         end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
         x = torch.cat([front, x, end], dim=1)
         x = self.avg(x.permute(0, 2, 1))
