e58bd1d494017355741c4547b7d9974dc9e39cbe
dg1609 <danielgrillmeyer@gmail.com>
2023-11-23 15:35:07 +0100

    changed kernel_size of TokenEmbedding from 3 -> 1 (bugfix)

--- layers/Embed.py
+++ layers/Embed.py
@@ -55,9 +55,9 @@ class PositionalEmbedding(nn.Module):
 class TokenEmbedding(nn.Module):
     def __init__(self, c_in, d_model):
         super(TokenEmbedding, self).__init__()
-        padding = 1 if compared_version(torch.__version__, '1.5.0') else 2
+        # padding = 1 if compared_version(torch.__version__, '1.5.0') else 2
         self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model,
-                                   kernel_size=1, padding=padding, padding_mode='replicate', bias=False)
+                                   kernel_size=1, padding=0, padding_mode='replicate', bias=False)
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
